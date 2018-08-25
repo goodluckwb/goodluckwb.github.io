65fd3d2eb50a886d3da6be8be47f491562ed066c
goodluckwb <weibinchina@163.com>
2018-08-25 20:03:43 +0800

    v1.17.2(8/25)

--- js/common.js
+++ js/common.js
@@ -2,10 +2,18 @@
 window.onscroll = function () {
     var totop = document.getElementById("go-to-top");
     var sc = document.documentElement.scrollTop || document.body.scrollTop;
-    totop.style.display = sc > 240 ? "block" : "none";
+    // totop.style.display = sc > 240 ? "block" : "none";
+    if(sc>240){
+        $(totop).fadeIn(500);
+		
+    }else{
+        $(totop).fadeOut(500);
+    }
 }
 function scrolltop() {
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    // document.body.scrollTop = document.documentElement.scrollTop = 0;
+    $('html,body').animate({scrollTop:0},500);
+    return false;
 }
 function randomColor(){
     var r=Math.floor(Math.random()*256);
@@ -16,76 +24,112 @@ function randomColor(){
 }
 $(document).ready(function(){
     //更换主题
-    $('#theme').change(function () {
+    $('#theme span').click(function(){
+        $('#theme ol').slideToggle(300);
+    })
+    $('#theme-select').click(function(){
+        $('#theme ol').slideToggle(300);
+    })
+    $('#theme ol li').click(function(){
+        var txt=$(this).text();
+        $('#theme-select').html(txt).attr('value',txt);        
+        changeTheme($(this));
+        $('#theme ol').hide(300);
+    })
+   function changeTheme(obj){
         var random=randomColor();
-        switch ($(this).val()) {
+        switch ($(obj).text()) {
             case '喜红':
-                $('#toptitle,nav,footer,footer span,#theme').css("background", "#CC0706");
-                $('h2, h3,article').css('border-color', '#CC0706');
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", "#CC0706");
+                $('h2, h3,article,#coolsites-list').css('border-color', '#CC0706');
                 $('h2,h3').css('color','#CC0706');
-                $('.title-div').css('border-bottom-color','#cc0706')
+                $('.title-div').css('border-bottom-color','#cc0706');
+                $('body,#coolsites-list').css('background','#FAE8E8')
                 break;
             case '深蓝':
-                $('#toptitle,nav,footer,footer span,#theme').css("background", "#0099CC");
-                $('h2,h3,article').css('border-color', '#0099CC');
-                $('h2,h3').css('color','#0099CC');
-                $('.title-div').css('border-bottom-color','#0099cc')
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", "#079FE3");
+                $('h2,h3,article,#coolsites-list').css('border-color', '#079FE3');
+                $('h2,h3').css('color','#079FE3');
+                $('.title-div').css('border-bottom-color','#079FE3')
+                $('body,#coolsites-list').css('background','linear-gradient(to right,#f9f9f9,#E8F6FC)');
                 break;
             case '豆绿':
-                $('#toptitle,nav,footer,footer span,#theme').css("background", "#99CC33");
-                $('h2,h3').css('border-color', '#99CC33');
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", "#99CC33");
+                $('h2,h3,article,#coolsites-list').css('border-color', '#99CC33');
                 $('h2,h3').css('color','#99CC33');
                 $('.title-div').css('border-bottom-color','#99cc33')
+                $('body,#coolsites-list').css('background','linear-gradient(to right,#f9f9f9,#F3F9E8)')
                 break;
             case '橙色':
-                $('#toptitle,nav,footer,footer span,#theme').css("background", "#FE9800");
-                $('h2,h3,article').css('border-color', '#FE9800');
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", "#FE9800");
+                $('h2,h3,article,#coolsites-list').css('border-color', '#FE9800');
                 $('h2,h3').css('color','#FE9800');
                 $('.title-div').css('border-bottom-color','#fe9800')
+                $('body,#coolsites-list').css('background','linear-gradient(to right,#f9f9f9,#FFF3E2)')
                 break;
             case '夜色':
-                $('#toptitle,nav,footer,footer span,#theme').css("background", "#1E1E1E");
-                $('h2,h3,article').css('border-color', '#1E1E1E');
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", "#1E1E1E");
+                $('h2,h3,article,#coolsites-list').css('border-color', '#1E1E1E');
                 $('h2,h3').css('color','#1E1E1E');
                 $('.title-div').css('border-bottom-color','#1e1e1e')
+                $('body,#coolsites-list').css('background','linear-gradient(to right,#f9f9f9,#E6E6E6)')
                 break;
             case '紫色':
-                $('#toptitle,nav,footer,footer span,#theme').css("background", "#9966cc");
-                $('h2,h3,article').css('border-color', '#9966cc');
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", "#9966cc");
+                $('h2,h3,article,#coolsites-list').css('border-color', '#9966cc');
                 $('h2,h3').css('color','#9966cc');
                 $('.title-div').css('border-bottom-color','#9966cc')
+                $('body,#coolsites-list').css('background','linear-gradient(to right,#f9f9f9,#F0E8F7)')
                 break;
             case '随机':
-                $('#toptitle,nav,footer,footer span,#theme').css("background", random);
-                $('h2,h3,article').css('border-color', random);
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", random);
+                $('h2,h3,article,#coolsites-list').css('border-color', random);
                 $('h2,h3').css('color',random);
                 $('.title-div').css('border-bottom-color',random)
                 break;
             default:
-                $('#toptitle,nav,footer,footer span,#theme').css("background", "#CC0706");
-                $('h2,h3,article').css('border-color', '#CC0706');
+                $('#toptitle,nav,footer,footer span,#theme ol').css("background", "#CC0706");
+                $('h2,h3,article,#coolsites-list').css('border-color', '#CC0706');
                 $('h2,h3').css('color','#CC0706');
                 $('.title-div').css('border-bottom-color','#cc0706')
+                $('body,#coolsites-list').css('background','linear-gradient(to right,#f9f9f9,#FAE8E8)')
                 break;
         }
-    })
+    }
 
      //publicmenu done.
-    $("#public-menu").click(function(){
-    $("#public-menu-content").slideToggle(500);
-    
+    $("#public-menu").click(function(e){
+		e.stopPropagation();
+		$("#public-menu-content").slideToggle(500);
+		if($("#public-menu-content").is(":visible")){
+		   $(document).one("click",function(){
+			$("#public-menu-content").slideUp(500);
+		   });
+		}
+	});
+	$("#public-menu-content").on("click",function(e){
+		e.stopPropagation();
+	})
+	
+	
+    $("#public-menu-content-div1>p").click(function(){
+    $("#public-menu-content-div1>ul").slideToggle(500);
     })
-    $("#public-menu-content").mouseleave(function(){
-        $("#public-menu-content").slideUp(500);
-        
+	$("#public-menu-content-div2>p").click(function(){
+    $("#public-menu-content-div2>ul").slideToggle(500);
     })
-    $("#public-menu-content div").click(function(){
-    $("#public-menu-content div ul").slideToggle(500);
-    })
-    $("#silder-music-show").click(function(){
-        $("#silder-music-content").animate({'width':'toggle'},1000)
+    //侧边音乐
+    $("#side-music-show").click(function(){
+        $("#side-music-content").animate({'width':'toggle'},1000)
     })
 
-
-
+    //coolsites-list
+    $('#coolsites').click(function(){
+        $('#coolsites-list').slideToggle(500);
+    })
+    $('#coolsites-list').mouseleave(function(){
+        $('#coolsites-list').slideUp(500);
+    })
+    //加载酷站
+    $('#coolsites-list').load('./source/html/coolsites.html');
 })
--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
     //文章点击按钮展开和收缩功能
     $("article i.fas").click(function () {
         var $pp = $(this).siblings('div:last')
+        
         if ($pp.css('display') == 'block') {
             $pp.hide(500);
-            $(this).removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+            $(this).removeClass('fa-angle-up').addClass('fa-angle-down').text('展开');
         } else {
             $pp.show(500);
-            $(this).removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+            $(this).removeClass('fa-angle-down').addClass('fa-angle-up').text('收起');
         }
     });
     //加载计算器    
@@ -16,9 +17,10 @@ $(document).ready(function () {
     $('#click-jsq').click(function(){
         $('#jisuanqi').slideToggle(500);
     })
+    $('#focus-content').load('./source/html/focus.html');
     //自动加载刷新股票
     function refreshPrice() {
-        var x = ['1000651', '1000333', '0600690'];
+        var x = ['1000651', '1000333', '0600690','0600887','0600519','0600030','1150153'];
         var url = 'https://api.money.126.net/data/feed/';
         for (let i = 0; i < x.length; i++) {
             url = url + x[i] + ',';
@@ -35,7 +37,7 @@ $(document).ready(function () {
                 let price = "<li><span>"+(i+1)+'</span> ' + data[x[i]].name + " " + data[x[i]].price + " " + (data[x[i]].percent * 100).toFixed(2) + '%' + data[x[i]].arrow + "</li>"
                 $("#current_price").append(price);
     
-                $("#current_price li")[i].style.color = data[x[i]].percent < 0 ? "#00FF00" : "#FF0000";
+                $("#current_price li")[i].style.color = data[x[i]].percent < 0 ? "#99cc33" : "#CC0706";
             }
         }).fail((xhr, status) => {
             console.log('失败: ' + xhr.status + ', 原因: ' + status);
@@ -46,23 +48,26 @@ $(document).ready(function () {
     //音乐盒
     var
         play = $('#musicplay'),
-        music = $('audio')[0];
+        music = $('audio')[0],
         pre = $('#musicprevious'),
         next = $('#musicnext'),
         title = $('#musictitle'),
-        cover = $('#musiccover'),
+        cover = $('#musiccover img'),
+
         wyurl="https://music.163.com/song/media/outer/url?id=";
     var obj = {
         "sites": [
-            { 'title': '只要平凡', 'name': '张杰 张碧晨', 'url': wyurl+'574919767' },
-            { 'title': 'I Do', 'name': '911', 'url': wyurl+'28256115' },
-            { 'title': '我好像在哪见过你', 'name': '薛之谦', 'url': wyurl+'417859631' },
-            { 'title': 'You Say', 'name': 'Lauren Daigle', 'url': wyurl+'575015416' },
-            { 'title': '致青春', 'name': '王菲', 'url': wyurl+'27588992' },
-            { 'title': 'Rivers of Love', 'name': 'Lisa Ekdahl', 'url': wyurl+'2923752' },
-            { 'title': '半壶纱', 'name': '刘珂矣', 'url': wyurl+'28793140' }
+            { 'title': '只要平凡', 'name': '张杰 张碧晨', 'url': wyurl+'574919767','coverurl':'https://p1.music.126.net/CjGoliP3xOB0gcCUaeTTBg==/109951163375727336.jpg?param=300x300' },
+            { 'title': 'I Do', 'name': '911', 'url': wyurl+'28256115','coverurl':'https://p1.music.126.net/ZNnDcq2-b_An3_ag3H5XhQ==/5980243743520718.jpg?param=300x300' },
+            { 'title': '我好像在哪见过你', 'name': '薛之谦', 'url': wyurl+'417859631','coverurl':'https://p1.music.126.net/hti_a0LADoFMBHvOBwAtRA==/1369991500930171.jpg?param=300x300' },
+            { 'title': 'You Say', 'name': 'Lauren Daigle', 'url': wyurl+'575015416' ,'coverurl':'https://p1.music.126.net/VWpkL_6bE5J44Cqe37RlCA==/109951163405345754.jpg?param=300x300'},
+            { 'title': '致青春', 'name': '王菲', 'url': wyurl+'27588992','coverurl':'https://p1.music.126.net/NGup83j86vv_IUsxJC7hDg==/5654788301727269.jpg?param=300x300' },
+            { 'title': '暗涌', 'name': '王菲', 'url': wyurl+'300136','coverurl':'https://p1.music.126.net/w8RFsMH8VJfPsBmVudYGsA==/109951163020569833.jpg?param=300x300' },
+            { 'title': 'Faraway', 'name': 'Gala', 'url': wyurl+'18059932','coverurl':'https://p1.music.126.net/ebiFHjGorTpqEihTNM2G3Q==/3231464674043130.jpg?param=200x200' },
+			{ 'title': 'Beautiful now', 'name': 'Zedd', 'url': wyurl+'32019002','coverurl':'http://p1.music.126.net/ze_ggtReuHBLF2o-wUolFw==/109951163221161145.jpg?param=300x300' },
+            { 'title': '半壶纱', 'name': '刘珂矣', 'url': wyurl+'28793140' ,'coverurl':'https://p1.music.126.net/SKujq5vqqv4KYOVFk7SiRA==/3407386538630284.jpg?param=300x300'}
         ]
-    }
+    };
     var mt = obj.sites[0].title + ' / ' + obj.sites[0].name;
     title.text(mt);
     // music.loop = true;
@@ -74,9 +79,10 @@ $(document).ready(function () {
         } else {
             play.removeClass('normal').addClass('pushdown');
             music.src = obj.sites[0].url;
+          
+            changeCover(0);
             music.play();
             rotate();
-           
             
         }
     });
@@ -89,7 +95,8 @@ $(document).ready(function () {
         mt = obj.sites[0].title + ' / ' + obj.sites[0].name;
         play.removeClass('normal').addClass('pushdown');
         music.play();
-        
+       
+        changeCover(0);
         title.text(mt);
         rotate();
     })
@@ -103,6 +110,8 @@ $(document).ready(function () {
         play.removeClass('normal').addClass('pushdown');
         music.play();
         title.text(mt);
+       
+        changeCover(0);
         rotate();
     })
     //进度条
@@ -112,12 +121,9 @@ $(document).ready(function () {
     var ss=obj.sites;
     var timer1;
     $('#jindutiao span').click(function(){
-        list.empty();
-        for(let i=0;i<len;i++){
-            var li='<li> '+ 0+(i+1) +' '+ss[i].title+" "+ss[i].name+'</li>';
-            list.append(li);
-        }
-        list.slideToggle(500); 
+
+    refreshplaylist();
+    list.slideToggle(500); 
         //闭包输出点击播放列表的歌曲
         function sequnce(arrLi){
             for(var i = 0; i < arrLi.length; i++){
@@ -129,6 +135,7 @@ $(document).ready(function () {
                         mt = obj.sites[j].title + ' / ' + obj.sites[j].name;
                         play.removeClass('normal').addClass('pushdown');
                         music.play();
+                        changeCover(j);
                         
                         title.text(mt);
                         rotate();
@@ -142,6 +149,14 @@ $(document).ready(function () {
         sequnce(lii);
        
     })
+    //播放列表刷新
+    function refreshplaylist(){
+        list.empty();
+        for(let i=0;i<len;i++){
+            var li='<li> '+ 0+(i+1) +' '+ss[i].title+" "+ss[i].name+'</li>';
+            list.append(li);
+        }}
+       
      //图片旋转，每30毫米旋转5度 和进度条
      function rotate(){
         
@@ -174,21 +189,37 @@ $(document).ready(function () {
     //图片暂停旋转
     function imagePause(){
         clearInterval(timer1);
-       
     }
-    
-    // function jindutiao(){
-    //     var jdt=$('#jindutiao-current')[0];
-    //     //获取当前歌曲的歌长
-    //     var lenth=music.duration;
-    //     timer1=setInterval(function(){
-    //         cur=music.currentTime;//获取当前的播放时间
-    //         jdt.style.width=""+parseFloat(cur/lenth)*300+"px";
-    //     },50)
-    // }
     //顺序播放
-    music.addEventListener("ended",imagePause(),true);
+    setInterval(function(){
+        if(music.ended){
+            next.click();
+        }
+    },1000)
+    //换封面
+    function changeCover(x){
+        // cover[0].src="";
+        cover[0].src=obj.sites[x].coverurl;
+    }
     //设置风格
+    
+    //canvas 作图标
+    function playicon(){
+        var cc=$('#myCanvas')[0];
+        var ctx=cc.getContext("2d");
+            ctx.beginPath();
+            ctx.moveTo(20,12);
+            ctx.lineTo(40,25);
+            ctx.lineTo(20,38);
+            ctx.closePath();
+            ctx.lineWidth=5;
+            ctx.linJion='round';
+            ctx.strokeStyle='white';
+            ctx.stroke();
+        
+
+    }
+
     //天气预报
     
     
@@ -230,7 +261,7 @@ $(document).ready(function () {
                 lili.empty();
                 $("#weather h4").html(' '+month + '月' + todayrq + " 星期" + todayxq +" "+we.forecasts[0].city);
                 var first = '<li>今日:高' + todaywe.daytemp + "°C 低" + todaywe.nighttemp
-                    + "°C " + todaywe.daywind + "风 " + fengli1 + '级 ' + todaywe.dayweather+'</li>';
+                    + "°C " + todaywe.daywind + "风" + fengli1 + '级 ' + todaywe.dayweather+'</li>';
                 lili.append(first);
                 var wei = we.forecasts[0].casts;
                 for (let i = 1; i < wei.length; i++) {
@@ -247,6 +278,69 @@ $(document).ready(function () {
        tianQi($(this).val(),'post','.box1');
    })
 
-  
+  //热点选项卡切换
+  function Sw(id1,id2){
+    this.tab=$(id1).find('li');
+    this.div=$(id2).find('div');
+    this.index=0;
+    
+  }
+  Sw.prototype.Switch=function(){
+     for(var i=0;i<this.tab.length;i++){
+          var that=this;
+          this.tab[i].index=i;
+          $(this.tab[i]).on('click',function(){
+            that.Tab(this);
+        })
+      }
+  }
+  Sw.prototype.Tab=function(obj){
+    for(let i=0;i<this.tab.length;i++){
+        $(this.tab[i]).removeClass();
+        $(this.div[i]).slideUp(500);
+    }
+    $(this.tab[obj.index]).addClass('active');
+   $(this.div[obj.index]).slideDown(500);
+  }
+//   function Sw(id1,id2){
+//     this.diva=document.getElementById(id1);
+//     this.tab=this.diva.getElementsByTagName('li');
+//     this.divb=document.getElementById(id2);
+//     this.div=this.divb.getElementsByTagName('div');
+//     this.index=0;
+//   }
+//   Sw.prototype.Switch=function(){
+//       for(let i=0;i<this.tab.length;i++){
+//           var that=this;
+//           this.tab[i].index=i;
+//           this.tab[i].onmouseover=function(){
+//             that.Tab(this);
+//           }
+//       }
+//   }
+//   Sw.prototype.Tab=function(obj){
+//     for(let i=0;i<this.tab.length;i++){
+//         this.tab[i].className='';
+//         this.div[i].style.display='none';
+//     }
+//     this.tab[obj.index].className='active';
+//     this.div[obj.index].style.display='block';
+//   }
+  Sw.prototype.autoplay=function(){
+        var that=this;
+        setInterval(function(){
+            if(that.index==that.tab.length-1){
+                that.index=0;
+            }else{
+                that.index++;
+            }
+        that.Tab(that);
+        },3000);    
+    }
+  window.onload=function(){
+      var tab1=new Sw($('#focus-list'),$('#focus-content'));
+      tab1.Switch();
+  }
+
 
 })
