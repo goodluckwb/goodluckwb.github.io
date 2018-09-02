6a89b2a2a4ae0e04ea9825572a7e075870e98ed9
goodluckwb <weibinchina@163.com>
2018-09-03 00:38:10 +0800

    v1.18.4

--- js/head.js
+++ js/head.js
@@ -1,4 +1,4 @@
-var head=document.getElementsByTagName('head');
+var head=document.getElementsByTagName('head')[0];
 var html='';
 html+='<meta charset="UTF-8">';
 html+='<meta name="viewport" content="width=device-width, initial-scale=1.0">';
@@ -9,11 +9,15 @@ html+='<link rel="stylesheet" href="./css/main.css">';
 html+='<link rel="stylesheet" href="./css/wbstyle.css">';
 html+='<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" crossorigin="anonymous">';
 html+='<script src="./js/jquery-3.3.1.min.js"></script>';
-html+='<script src="https://unpkg.com/react@16/umd/react.development.js"></script>';
-html+='<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>';
-html+='<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>';
+html+='<script src="./js/react.development.js"></script>';
+html+='<script src="./js/react-dom.development.js"></script>';
+html+='<script src="./js/babel.min.js"></script>';
 html+='<script src="./js/nav.jsx" type="text/babel"></script>';
 html+='<script src="./js/common.js"></script>';
 html+='<script src="./js/jisuanqi.js"></script>';
+document.open();
 document.write(html);
+
+
+
 
